d5ef1823fd524f8265e31e98d0cb881296581d10
Paul Schevelenbos <paul.schevelenbos@telenet.be>
2021-01-07 15:58:21 +0100

    test fetch() met USER_013 na npm cors

--- FI.js
+++ FI.js
@@ -7,14 +7,9 @@ class FI {
 
     async createOnLedger() {
 
-        //  curl --request POST http://127.0.0.1:8080/api/enrollAdminOrg1
+        /*
+        //  TEST1: curl --request POST http://127.0.0.1:8080/api/enrollAdminOrg1
         const enrollAdminORG1URL = 'http://127.0.0.1:8080/api/enrollAdminOrg1';
-
-        // curl --request POST --data '{"userId":"USER_005","password":"0x023","companyId":"COMP_001","email":"james.bond@mi6.org","firstname":"James","lastname":"Bond"}' -H "Content-Type: application/json"  http://127.0.0.1:8080/api/createUser;
-        // const createUserURL = 'http://127.0.0.1:8080/api/createUser';
-
-        // call REST API
-        // const data = { "userId": "USER_006", "password": "0x023", "companyId": "COMP_001", "email": "james.bond@mi6.org", "firstname": "James", "lastname": "Bond" };
         const response = await fetch(enrollAdminORG1URL, {
             method: 'POST',
             mode: 'cors',
@@ -25,9 +20,27 @@ class FI {
             // referrerPolicy: 'Access-Control-Allow-Origin',
             // body: JSON.stringify(data)
         });
+        console.log("response to createOnLedger -> fetch(): ", response);
+        // const responseData = await response.json();
+        */
+
+        // TEST2: curl --request POST --data '{"userId":"USER_001","password":"0x023","companyId":"COMP_001","email":"james.bond@mi6.org","firstname":"James","lastname":"Bond"}' -H "Content-Type: application/json"  http://127.0.0.1:8080/api/createUser;
+        const createUserURL = 'http://127.0.0.1:8080/api/createUser';
+        const data = { "userId": "USER_013", "password": "0x023", "companyId": "COMP_001", "email": "james.bond@mi6.org", "firstname": "James", "lastname": "Bond" };
+        const response = await fetch(createUserURL, {
+            method: 'POST',
+            mode: 'cors',
+            //cache: 'default',
+            // credentials: 'same-origin',
+            headers: { 'Content-Type': 'application/json' },
+            // headers: { 'Content-Type': 'application/json' , 'Access-Control-Allow-Origin': '*'},
+            // redirect: 'follow',
+            // referrerPolicy: 'Access-Control-Allow-Origin',
+            body: JSON.stringify(data)
+        });
         console.log("body parameter: ", JSON.stringify(data));
         console.log("response to createOnLedger -> fetch(): ", response);
-        // const data = await response.json();
+        // const responseData = await response.json();
 
     }
 }
