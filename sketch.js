77e3074205cd60c8029a681fea69fbac636d2071
Paul Schevelenbos <paul.schevelenbos@telenet.be>
2021-01-23 23:18:18 +0100

    Bootstrap with tabs implemented

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ const colorLocationField = [252, 159, 159, 150];
 // Simulation parameters
 let evCount = 5; // number of EV's
 let nLocations = 1; // number of locations with EVSEs, topped to myLocations.maxLocations()
-let evseDistribution = [5, 2, 1]; // maximum number of EVSEs per location
+let evseDistribution = [10, 5, 2, 1]; // maximum number of EVSEs per location
 let objectSize; // size of EV's and CP's in pixels for display
 let speedSlider; // slider element in the DOM: minutes per frame (1 to 60)
 let maxSpeed = 5; // Maximum speed: 5 to 15
@@ -27,8 +27,9 @@ let maxForce = 2.0; // Maximum steering force: 2.0 to 4.0
 let simulationFrameRate = 60; // 60 frames per second (every frame is from 1 minute up to 60 minutes)
 let simulationClock; // keeps track of (accellerated) time progression
 let startStopButton; // alternates between start/resume or stop 
-let resetButton; // resets the currentTime to startTime
-let simulationRunning = false; // function draw only executes when 'true'
+// let resetButton; // resets the currentTime to startTime
+let simulationRunning = false; // function draw() only executes when 'true'
+let simulationParametersSet = false; // function setup() only executes when 'true'
 let myLocations; // territory split in locations like a chessfield
 let myResolution; // pixel length of one matrix square dividing the map in locations
 
@@ -60,56 +61,60 @@ let myTech; // Tech account keeps track of created €-tokens
 // preparation before draw
 async function setup() {
 
-	// createCanvas(windowWidth, windowHeight)
-	cityMap = loadImage('images/Medium-MapLeuven.jpg'); // TBC zoomed-out image when many EV's
-	var canvas = createCanvas(787, 528);
-	canvas.parent('jumbo-canvas');
+	if (simulationParametersSet) {
+		// createCanvas(windowWidth, windowHeight)
+		cityMap = loadImage('images/Medium-MapLeuven.jpg'); // TBC zoomed-out image when many EV's
+		var canvas = createCanvas(787, 528);
+		canvas.parent('jumbo-canvas');
 
 
-	// time elements
-	speedSlider = createSlider(1, 60, 1, 1); // createSlider(min, max, [value], [step])
-	frameRate(simulationFrameRate);
-	simulationClock = new Clock(new Date(2021, 02, 01, 9, 30));
-	startStopButton = createButton('START/STOP'); // to start/stop the simulation run 
-	startStopButton.mousePressed(startStopSimulation);
+		// time elements
+		speedSlider = createSlider(1, 60, 1, 1); // createSlider(min, max, [value], [step])
+		speedSlider.parent('jumbo-canvas');
+		frameRate(simulationFrameRate);
+		simulationClock = new Clock(new Date(2021, 02, 01, 9, 30));
+		startStopButton = createButton('START/STOP'); // to start/stop the simulation run 
+		startStopButton.parent('jumbo-canvas');
+		startStopButton.mousePressed(startStopSimulation);
 
-	// choose objectSize (ChargePoint, EV) and resolution of locations proportional with evCount
-	objectSize = round(map(evCount, 0, 1000, 9, 3, true));
-	myResolution = 5 * objectSize;
-	myLocations = new Locations(myResolution);
+		// choose objectSize (ChargePoint, EV) and resolution of locations proportional with evCount
+		objectSize = round(map(evCount, 0, 1000, 9, 3, true));
+		myResolution = 5 * objectSize;
+		myLocations = new Locations(myResolution);
 
-	// create stakeholders, Locations and chargepoints
-	myMSP = new MSP("TRES", 0.01); // specify name and fee
-	const mspCreatedOnLedger = await myMSP.createOnLedger();
-	myFI = new FI("KBC", 0.01); // specify name and fee
-	const fiCreatedOnLedger = await myFI.createOnLedger();
-	myTech = new TechAccount();
-	const techCreatedOnLedger = await myTech.createOnLedger();
+		// create stakeholders, Locations and chargepoints
+		myMSP = new MSP("TRES", 0.01); // specify name and fee
+		const mspCreatedOnLedger = await myMSP.createOnLedger();
+		myFI = new FI("KBC", 0.01); // specify name and fee
+		const fiCreatedOnLedger = await myFI.createOnLedger();
+		myTech = new TechAccount();
+		const techCreatedOnLedger = await myTech.createOnLedger();
 
-	nLocations = Math.min(nLocations, myLocations.maxLocations()); // no more than grid allows
-	for (let i = 0; i < nLocations; i++) {
-		let newCPO = new CPO(i);
-		myCPOs.push(newCPO);
-		const cpoCreatedOnLedger = await newCPO.createOnLedger();
-		// create new location
-		let newLocation = myLocations.addLocation(newCPO.getID());
-		// create chargepoints (EVSEs) in this new location (number according to distribution)
-		let nEVSEonLocation = random(evseDistribution);
-		for (let j = 0; j < nEVSEonLocation; j++) {
-			let newEVSEid = newLocation.getName() + "/" + newCPO.getName() + "/" + String(j);
-			let newEVSE = new EVSE(newCPO, newEVSEid, newLocation, myMSP);
-			newLocation.addEVSE(newEVSE);
-			myMSP.addEVSE(newEVSE);
-			newCPO.addEVSE(newEVSE);
+		nLocations = Math.min(nLocations, myLocations.maxLocations()); // no more than grid allows
+		for (let i = 0; i < nLocations; i++) {
+			let newCPO = new CPO(i);
+			myCPOs.push(newCPO);
+			const cpoCreatedOnLedger = await newCPO.createOnLedger();
+			// create new location
+			let newLocation = myLocations.addLocation(newCPO.getID());
+			// create chargepoints (EVSEs) in this new location (number according to distribution)
+			let nEVSEonLocation = random(evseDistribution);
+			for (let j = 0; j < nEVSEonLocation; j++) {
+				let newEVSEid = newLocation.getName() + "/" + newCPO.getName() + "/" + String(j);
+				let newEVSE = new EVSE(newCPO, newEVSEid, newLocation, myMSP);
+				newLocation.addEVSE(newEVSE);
+				myMSP.addEVSE(newEVSE);
+				newCPO.addEVSE(newEVSE);
+			}
 		}
-	}
 
-	// create Electronic Vehicles
-	for (let i = 0; i < evCount; i++) {
-		let newEV = new EV(i);
-		myFleet.push(newEV);
-		const evCreatedOnLedger = await newEV.createOnLedger();
-		const evFundedInitially = await newEV.fund(initialFunding);
+		// create Electronic Vehicles
+		for (let i = 0; i < evCount; i++) {
+			let newEV = new EV(i);
+			myFleet.push(newEV);
+			const evCreatedOnLedger = await newEV.createOnLedger();
+			const evFundedInitially = await newEV.fund(initialFunding);
+		}
 	}
 };
 
